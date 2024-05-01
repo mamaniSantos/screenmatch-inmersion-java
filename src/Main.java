437c08704ad6346689fd820fccc9a1d91801987d
github.com/mamaniSantos <mamani.santos@hotmail.com>
2024-05-01 11:26:00 -0300

    aula 01 inmersion java

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a la inmersion en Java");
+        //System.out.println("Pelicula Matrix");
+
+        //Declaracion de variables
+
+        int fechaDeLanzamineto = 1999;
+        double evaluacion = 4.5;
+        boolean incluidoEnElPlanBasico = true;
+        String nombre = "Matrix";
+        String sinopsis = """ 
+                La mejor peliucla del milenio
+                """;
+        double mediaEvaluacionUsuario=0;
+
+        System.out.println("Peliculas " + nombre);
+        System.out.println(fechaDeLanzamineto);
+        System.out.println(evaluacion);
+        System.out.println(incluidoEnElPlanBasico);
+
+        double mediaEvaluacion = (4.5 + 4.8 + 3) / 3;
+        System.out.println("Media de la evalucion de Matrix " + mediaEvaluacion);
+
+        if (fechaDeLanzamineto >= 2023) {
+
+            System.out.println("Pelicula popular del momento");
+
+        } else {
+            System.out.println("Pelicula Retro");
+        }
+
+        for (int i = 0; i < 3; i++) {
+
+            Scanner teclado=new Scanner(System.in);
+            System.out.println("Ingresa la nota que le darias a Matrix");
+            double notaMatrix= teclado.nextDouble();
+
+            mediaEvaluacionUsuario = (mediaEvaluacionUsuario + notaMatrix);
+
+
+        }
+        System.out.println("La media de la pelicula calculada "+mediaEvaluacionUsuario/3);
+    }
+
+
+
+
+}
